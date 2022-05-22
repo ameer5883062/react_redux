c2cc315b2686dda27d70b43eabe68919b8a86145
ameer5883062 <ameer5883062@gmail.com>
2022-05-22 19:26:32 +0500

    react_redux_third_commit

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -5,65 +5,72 @@ import Header from "./Header";
 const Home = () => {
   const Products = [
     {
-      image: "images/image_1.png",
+      id: 1,
+      image: "http://localhost:3000/images/image_1.png",
       name: "Man Joggers",
       price: "1999",
     },
     {
-      image: "images/image_2.png",
+      id: 2,
+      image: "http://localhost:3000/images/image_2.png",
       name: "Man Coat",
       price: "2499",
     },
     {
-      image: "images/image_3.jpg",
+      id: 3,
+      image: "http://localhost:3000/images/image_3.jpg",
       name: "Realmi Mobile",
       price: "24999",
     },
     {
-      image: "images/image_4.jpg",
+      id: 4,
+      image: "http://localhost:3000/images/image_4.jpg",
       name: "Rolex Watche",
       price: "999",
     },
   ];
 
   return (
-    <>
+    <div>
       <Header />
 
       <Container className="mt-5">
         <Row>
-          {Products.map((item) => (
-            <Col lg={3} md={6} sm={12}>
-              <Card className="Card shadow-lg">
-                <Card.Img
-                  variant="top"
-                  className="CardImage"
-                  src={item.image}
-                />
-                <hr className="mx-3" />
-                <Card.Body className="mt-0 pt-0 mx-1">
-                  <Row>
-                    <Col>
-                      <Card.Title className="ProductName">
-                        {item.name}
-                      </Card.Title>
-                    </Col>
-                    <Col className="text-end">
-                      <Card.Title className="ProductPrice">
-                        ${item.price}
-                      </Card.Title>
-                    </Col>
-                  </Row>
-                  <Row className="mt-2">
-                    <Button variant="dark">Add To Cart</Button>
-                  </Row>
-                </Card.Body>
-              </Card>
-            </Col>
-          ))}
+          {Products &&
+            Products.map((item) => (
+              <Col lg={3} md={6} sm={12}>
+                <div key={item.id}>
+                  <Card className="Card shadow-lg">
+                    <Card.Img
+                      variant="top"
+                      className="CardImage"
+                      src={item.image}
+                    />
+                    <hr className="mx-3" />
+                    <Card.Body className="mt-0 pt-0 mx-1">
+                      <Row>
+                        <Col>
+                          <Card.Title className="ProductName">
+                            {item.name}
+                          </Card.Title>
+                        </Col>
+                        <Col className="text-end">
+                          <Card.Title className="ProductPrice">
+                            ${item.price}
+                          </Card.Title>
+                        </Col>
+                      </Row>
+                      <Row className="mt-2">
+                        <Button variant="dark">Add To Cart</Button>
+                      </Row>
+                    </Card.Body>
+                  </Card>
+                </div>
+              </Col>
+            ))}
         </Row>
       </Container>
-    </>
+    </div>
   );
 };
 
---
+++ src/Services/Constants.js
@@ -0,0 +1,2 @@
+// ADD_TO_CART
+export const ADD_TO_CART = "ADD_TO_CART";
